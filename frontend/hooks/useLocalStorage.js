3130df66bee5eea541cfcdc1680e37d28034119f
Megan Huckabay <huckabay90@gmail.com>
2023-08-21 19:12:00 -0700

    finished task

---
+++ frontend/hooks/useLocalStorage.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+const useLocalStorage = (key, initialValue) => {
+  const storedValue = localStorage.getItem(key);
+  const initial = storedValue !== null ? JSON.parse(storedValue) : initialValue;
+
+  const [value, setValue] = useState(initial);
+
+  const updateValue = (newValue) => {
+    setValue(newValue);
+    localStorage.setItem(key, JSON.stringify(newValue));
+  };
+
+  return [value, updateValue];
+};
+
+export default useLocalStorage;
